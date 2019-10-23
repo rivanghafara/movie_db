4ae33fda29a0c8301c8728c49d0a495ad0930068
Rivan Ghafara Devrianza <rgdevrianza@gmail.com>
2019-10-23 09:28:48 +0700

    Feature/create small component (#2)
    * add and create small component

---
+++ src/components/popular-movies/Movie.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import GridList from '@material-ui/core/GridList';
+import MovieItem from '../movie-item/movie-item'
+
+const PopularMovie = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=850c683da4d46367b8a14773ea9219a0&language=en-US&region=US&page=1')
+                setMovies(result.data.results)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchData()
+    }, [])
+
+    return (
+
+        movies
+            .map((item) => (
+                <GridList key={item.id}>
+                    <MovieItem  id={item.id} title={item.title} posterImg={item.poster_path} overview={item.overview} />
+                </GridList>
+            ))
+    )
+}
+
+export default PopularMovie
