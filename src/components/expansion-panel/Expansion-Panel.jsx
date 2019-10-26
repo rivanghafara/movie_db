c89ca88ea7c7ce3982787569ed59080798d1f962
rivan <rgdevrianza>
2019-10-26 15:12:52 +0700

    not done yet

--- src/components/expansion-panel/Expansion-Panel.jsx
+++ src/components/expansion-panel/Expansion-Panel.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CheckboxGroup from '../checkbox/CheckboxGroup'
 
-import Checkbox from '../../components/checkbox/Checkbox'
+import axios from 'axios'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,8 +19,39 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+
+
 export default function SimpleExpansionPanel() {
   const classes = useStyles();
+  const [genres, setGenres] = useState([])
+  var arrGenres = []
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=850c683da4d46367b8a14773ea9219a0&language=en-US')
+        setGenres(result.data.genres)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData()
+  }, [])
+
+  const handleChange = event => {
+    var index = arrGenres.indexOf(event.target.value)
+    if (index !== -1) {
+      if (event.target.checked === false) {
+        arrGenres.splice(index, 1);
+      }
+    } else {
+      arrGenres.push(event.target.value)
+
+    }
+
+    console.log(arrGenres);
+
+  }
 
   return (
     <div className={classes.root}>
@@ -32,7 +64,20 @@ export default function SimpleExpansionPanel() {
           <Typography className={classes.heading}>Expansion Panel 1</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Checkbox />
+
+
+          {console.log(genres)}
+          <CheckboxGroup onClick={handleChange} key='1' value='hai'/>
+          
+          {/* {genres.map((item) =>
+            <CheckboxGroup onClick={handleChange} key={item.id} {...item} />
+          )} */}
+          {/* <FormGroup row>
+            {console.log(genres)}
+            {genres.map((item) =>
+              <Checkbox key={item.id} label={item.name}/>
+            )}
+          </FormGroup> */}
         </ExpansionPanelDetails>
       </ExpansionPanel>
     </div>
