f4e5b8b7403eca7904c79da30132907096c1465b
rivan <rgdevrianza>
2020-03-01 09:39:44 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
-import TvPage from './pages/Homepage/TvHomepage'
 import Example from './pages/example';
-// import Movie from './components/Movie'
 import MovieDetails from './pages/Movies-Page/Movie-Details'
-import TvDetails from './pages/TV-Page/TV-Details'
 
 
 function App() {
@@ -17,8 +14,6 @@ function App() {
       <Route exact path='/movies' component={Homepage} />
       <Route exact path='/movies/:id' component={MovieDetails} />
       <Route exact path='/example' component={Example} />
-      {/* <Route exact path='/tv'component={TvPage} />
-      <Route exact path='/tv/:id'component={TvDetails} /> */}
     </Switch>
   );
 }
